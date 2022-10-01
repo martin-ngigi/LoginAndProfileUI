18d7e56bba496ee30bb2351c5f67dc3a30b34925
martin-ngigi <martinwainaina001@gmail.com>
2022-10-01 12:16:41 +0300

    Added dashboard

--- app/src/main/java/com/example/loginandprofileui/LoginActivity.java
+++ app/src/main/java/com/example/loginandprofileui/LoginActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextView createAccount;
+    private Button signInBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +19,19 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         createAccount =findViewById(R.id.createAccount);
+        signInBtn = findViewById(R.id.signInBtn);
+
         createAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
             }
         });
     }
--- app/src/main/java/com/example/loginandprofileui/RegisterActivity.java
+++ app/src/main/java/com/example/loginandprofileui/RegisterActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private TextView haveAccount;
+    private Button registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +18,14 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         haveAccount = findViewById(R.id.haveAccount);
+        registerBtn = findViewById(R.id.registerBtn);
+
         haveAccount.setOnClickListener( e->{
             startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+        });
+
+        registerBtn.setOnClickListener( e->{
+            startActivity(new Intent(RegisterActivity.this, HomeActivity.class));
         });
     }
 }
